3236fb12f58d5f68baab5a88fd5977d0559b2ac4
Gabriel Youbissi <youbigabriel@gmail.com>
2026-04-03 16:14:32 +0200

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int len=nums.length;
+        if(len==1) return nums[0];
+        if(len==2) return Math.max(nums[0], nums[1]);
+        int[] dp=new int[len];
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        dp[2]=dp[0]+nums[2];
+        for(int i=3; i<len; i++){
+            dp[i]=nums[i]+Math.max(dp[i-2], dp[i-3]);
+        }
+        return Math.max(dp[len-2], dp[len-1]);
+    }
+}
