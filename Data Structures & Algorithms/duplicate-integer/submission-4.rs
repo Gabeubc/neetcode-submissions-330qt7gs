a88c5a47a2042984193666b9915de27e6bde6c55
Gabriel Youbissi <youbigabriel@gmail.com>
2026-04-22 16:51:38 +0200

    Remove: duplicate-integer - submission-4

--- Data Structures & Algorithms/duplicate-integer/submission-4.rs
+++
@@ -1,6 +0,0 @@
-impl Solution {
-    pub fn has_duplicate(nums: Vec<i32>) -> bool {
-        let mut set:HashSet<i32>=nums.iter().cloned().collect();
-        return set.len()!=nums.len();
-    }
-}
