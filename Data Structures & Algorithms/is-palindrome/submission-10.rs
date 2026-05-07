ae0b6b4b8383d76a4cd034c9e767c2e0a56cc9da
Gabriel Youbissi <youbigabriel@gmail.com>
2026-05-07 12:24:42 +0200

    Remove: is-palindrome - submission-10

--- Data Structures & Algorithms/is-palindrome/submission-10.rs
+++
@@ -1,8 +0,0 @@
-impl Solution {
-    pub fn is_palindrome(s: String) -> bool {
-        let chars=s.chars()
-        .filter(|c| c.is_alphanumeric())
-        .map(|c| c.to_ascii_lowercase());
-        return chars.clone().eq(chars.rev());
-    }
-}
