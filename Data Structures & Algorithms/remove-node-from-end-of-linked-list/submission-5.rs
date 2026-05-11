0b834f02ff06f94ad35ed8f018a0e2856506ec28
Gabriel Youbissi <youbigabriel@gmail.com>
2026-05-11 21:03:31 +0200

    Add: remove-node-from-end-of-linked-list - submission-5

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-5.rs
@@ -0,0 +1,35 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//     pub val: i32,
+//     pub next: Option<Box<ListNode>>,
+// }
+//
+// impl ListNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         ListNode { next: None, val }
+//     }
+// }
+
+impl Solution {
+    pub fn remove_nth_from_end(mut head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let mut len=0;
+        let mut curr=head.as_ref();
+        while let Some(node)=curr{
+            len+=1;
+            curr=node.next.as_ref();
+        }
+        if len<=n{
+            return head.unwrap().next;
+        }
+        let mut curr_mut = head.as_mut();
+        for _ in 0..(len - n - 1) {
+            curr_mut = curr_mut.unwrap().next.as_mut();
+        }
+        let mut node_to_remove = curr_mut.as_mut().unwrap().next.take();
+        curr_mut.unwrap().next = node_to_remove.unwrap().next.take();
+        return head;
+        
+    }
+}
