a07b460f2f80b8fd4b6882415d1c6fdcecba2067
Gabriel Youbissi <youbigabriel@gmail.com>
2026-05-16 14:58:45 +0200

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1=s1.length();
+        int len2=s2.length();
+        if(len1>len2) return false;
+        int[] countSort1=new int[26];
+        int[] countSort2=new int[26];
+        for(int i=0; i<len1; i++){
+            countSort1[s1.charAt(i)-'a']+=1;
+            countSort2[s2.charAt(i)-'a']+=1;
+        }
+        for(int i=len1; i<len2; i++){
+            if(matches(countSort1, countSort2)) return true;
+            countSort2[s2.charAt(i)-'a']+=1;
+            countSort2[s2.charAt(i-len1)-'a']-=1;
+        }
+        return matches(countSort1, countSort2);
+    }
+    boolean matches(int[] s1, int[] s2){
+        for(int i=0; i<26; i++){
+            if(s1[i]!=s2[i]) return false;
+        }
+        return true;
+    }
+
+}
